1d2cebe92dd3c7bb50baf535b9a2d87c4f286d25
cybermerlin <1460479+cybermerlin@users.noreply.github.com>
2022-10-24 23:49:23 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import { Counter } from './features/counter/Counter';
 import "./App.css";
 import GridView from "./plugins/grid/View";
