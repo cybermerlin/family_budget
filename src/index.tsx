181d8264ef80b9ddc6e49b72aa6526621f451487
cybermerlin <1460479+cybermerlin@users.noreply.github.com>
2022-12-04 00:26:54 +0300

    mod: ts> upgrade to 4.9* and migrate css to scss (#28)

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
-import './index.css';
+import './index.scss';
 import App from './app/App';
 import { store } from './app/store';
 import reportWebVitals from './reportWebVitals';
