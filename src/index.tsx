915313778ce6711bc1fc6131a105e51e31b81ada
merlin <cybermerlin@ya.ru>
2022-12-19 21:44:45 +0300

    add: GraphQL-client> try 1 - just example to use appolo client for GQL

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,53 @@
+import { ApolloClient, ApolloProvider, gql, HttpLink, InMemoryCache } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-
-import './index.scss';
+import CONFIG from 'src/config.json';
 import App from './app/App';
 import { store } from './app/store';
+
+import './index.scss';
+import './lib/utils';
 import reportWebVitals from './reportWebVitals';
 
+
+const CLIENT = new ApolloClient({
+  link: new HttpLink({
+    uri: CONFIG.GRAPHQL
+    // eslint-disable-next-line capitalized-comments
+    // headers: { authorizetion: `Bearer ${process.env.GITHUB_TOKEN}` }
+  }),
+  cache: new InMemoryCache
+});
+
+const MISSIONS_QUERY = gql`
+  query GetLocations {
+    locations {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
+
+CLIENT.query({
+  query: MISSIONS_QUERY
+})
+      .then(console.info);
 
 let root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 
 root.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <App/>
-      </Provider>
-    </React.StrictMode>
+    <ApolloProvider client={CLIENT}>
+      <React.StrictMode>
+        <Provider store={store}>
+          <App/>
+        </Provider>
+      </React.StrictMode>
+    </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
